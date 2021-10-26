82e72e4edebd2495368b5fc0d15decd76142f1b0
ummay-kulsum18 <92802181+ummay-kulsum18@users.noreply.github.com>
2021-10-26 20:18:49 +0600

    Create Accountinfo.java

---
+++ Beans/Accountinfo.java
@@ -0,0 +1,52 @@
+package com.springapp.mvc.beans;
+
+/**
+ * Created by Jui on 21/5/2017.
+ */
+public class Accountinfo {
+    private String fname;
+    private String lname;
+    private String email;
+    private String uname;
+    private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public String getUname() {
+        return uname;
+    }
+
+    public void setUname(String uname) {
+        this.uname = uname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
